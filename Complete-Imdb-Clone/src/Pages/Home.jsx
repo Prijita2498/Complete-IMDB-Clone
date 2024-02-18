9b88e2a89ea13d6067250b390d93fb00f4e7ad91
prijitha unnikrishnan <drashtimedia98@gmail.com>
2024-02-18 16:49:43 +0530

    Api urls added for popular, top rated, upcoming and now playing movies

--- Complete-Imdb-Clone/src/Pages/Home.jsx
+++ Complete-Imdb-Clone/src/Pages/Home.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
-import Header from '../Components/Common/Header'
+import React, {useEffect, useState} from 'react';
+import Header from '../Components/Common/Header';
+import { categoryMovies} from '../Services/api';
+import { NOWPLAYING_API_URL } from '../assets/Constants';
 
 function Home() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=>{
+    const fetchData = async() =>{
+      let response = await categoryMovies(NOWPLAYING_API_URL);
+      console.log(response);
+    }
+
+    fetchData();
+  },[]);
+
   return (
     <div>
       <Header/>
--- Complete-Imdb-Clone/src/assets/Constants.jsx
+++ Complete-Imdb-Clone/src/assets/Constants.jsx
@@ -1,1 +1,6 @@
 export const logoURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"
+
+export const NOWPLAYING_API_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=028bafa5b74c7294bd42cfe872073ad7&language=en-US&page=1";
+export const  UPCOMING_API_URL = "https://api.themoviedb.org/3/movie/upcoming?api_key=028bafa5b74c7294bd42cfe872073ad7&language=en-US&page=1";
+export const TOPRATED_API_URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=028bafa5b74c7294bd42cfe872073ad7&language=en-US&page=1";
+export const  POPULAR_API_URL= "https://api.themoviedb.org/3/movie/popular?api_key=028bafa5b74c7294bd42cfe872073ad7&language=en-US&page=1";
